b797113cbcd2abf7996d02fceaeb56f182f48997
Sigazen <choikdee50@gmail.com>
2024-03-01 16:45:05 +0700

    add authentication for Registered User

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
-const dentists = require('./routes/dentists')
+const dotenv = require('dotenv');
+const dentists = require('./routes/dentists');
+const auth = require('./routes/auth');
+const cookieParser = require('cookie-parser');
+dotenv.config({path:'./config/config.env'});
+
+const connectDB = require('./config/db');
+connectDB();
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 
 app.use('/api/v1/dentist',dentists);
+app.use('/api/v1/auth',auth);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT,console.log("connecting"));
 
 process.on('unhandledRejection',(err,promise) =>{
