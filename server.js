c074f43fd76edcbfbf3f8c8efac0500ff43778bc
RGuide <bunditsrisakul@gmail.com>
2024-03-03 04:19:46 +0700

    update server.js

--- server.js
+++ server.js
@@ -3,31 +3,29 @@ const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 dotenv.config({ path: './config/config.env' });
 
-//Route files
-const dentists = require('./routes/dentists');
-const auth = require('./routes/auth');
-const appointments = require('./routes/appointment');
-
-//TODO
-//mongoSanitize
 const mongoSanitize = require('express-mongo-sanitize');
-//helmet
 const helmet = require('helmet');
-//xss
 const {xss} = require('express-xss-sanitizer');
-//rateLimit
 const rateLimit = require('express-rate-limit');
-//hpp
 const hpp = require('hpp');
-//cors
 const cors = require('cors');
-//Swagger
+
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUI = require('swagger-ui-express');
 
+// Import routes
+const dentists = require('./routes/dentists');
+const auth = require('./routes/auth');
+const appointment = require('./routes/appointment');
+
+// Load environment variables
+dotenv.config({ path: './config/config.env' });
+
+// Connect to database
 const connectDB = require('./config/db');
 connectDB();
 
+// Create Express app
 const app = express();
 
 const swaggerOptions = {
@@ -51,32 +49,32 @@ const swaggerDocs = swaggerJsDoc(swaggerOptions);
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 app.use(express.json());
+
+// Middleware
+app.use(cookieParser());
+
 //sanitize
 app.use(mongoSanitize());
-//helmet
 app.use(helmet());
-//
 app.use(xss());
-//
 const limiter = rateLimit({
-    windowsMs : 10*60*1000, //10 นาที
+    windowsMs : 10*60*1000, //10 mins
     max  : 100
 })
 app.use(limiter);
-//
 app.use(hpp());
-//
 app.use(cors());
 
-app.use(cookieParser());
-
+// Routes
 app.use('/api/v1/dentists',dentists);
 app.use('/api/v1/auth',auth);
 app.use('/api/v1/appointments', appointments);
 
+// Start the server
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT,console.log("connecting"));
 
+// Error handling
 process.on('unhandledRejection',(err,promise) =>{
     console.log(`Error: ${err.message}`);
     server.close(() => process.exit(1))
