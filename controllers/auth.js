67f43ae841d4f5f455feba6021289b0e062debd7
Alknot1122 <117248001+Alknot1122@users.noreply.github.com>
2024-03-02 16:26:06 +0700

    Fixed User Register Crash After Register

--- controllers/auth.js
+++ controllers/auth.js
@@ -27,8 +27,6 @@ exports.register = async (req, res, next) => {
             tel,
             role
         });
-        const token = user.getSignedJwtToken();
-        res.status(200).json({success: true, token});
         sendTokenResponse(user, 200, res);
     } catch(err) {
         res.status(400).json({success: false});
